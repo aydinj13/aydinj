fffae0542f42d9cb1891777755cfd877c970e21e
Aydin Joshi <ajoshi1336@gmail.com>
2024-03-21 10:03:50 -0400

    Intro change text

--- components/intro.tsx
+++ components/intro.tsx
@@ -64,9 +64,7 @@ export default function Intro() {
       >
         <span className="font-bold">Hello, I'm Aydin.</span> I'm a{" "}
         <span className="font-bold">full-stack developer</span> with{" "}
-        <span className="font-bold">4 years</span> of experience. I enjoy
-        building <span className="italic">sites & apps</span>. My focus is{" "}
-        <span className="underline">React (Next.js)</span>.
+        <span className="font-bold">4 years</span> of experience in sofware.
       </motion.h1>
 
       <motion.div
